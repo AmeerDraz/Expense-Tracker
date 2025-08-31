001b323761678d9d33d2f6839ae3eb10bffb209b
AmeerDraz <ameerabudraz1@gmail.com>
2025-08-31 15:23:08 +0300

    feat: login ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                display: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                primary: "#875cf5",
+            },
+            screens: {
+                "3xl": "1920px",
+            },
+        },
     },
     plugins: [],
 };
