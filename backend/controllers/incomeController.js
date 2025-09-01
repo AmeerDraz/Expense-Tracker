29dc749078a85c27b09de349294fa965637dd85a
AmeerDraz <ameerabudraz1@gmail.com>
2025-09-01 15:45:04 +0300

    to: 1:22:55

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,11 @@
+// Add Income Source
+exports.addIncome = async (req, res) => {};
+
+// get All Income Source
+exports.getAllIncome = async (req, res) => {};
+
+// delete Income Source
+exports.deleteIncome = async (req, res) => {};
+
+// dowmload Excel
+exports.dowmloadIncomeExcel = async (req, res) => {};
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,16 +2,17 @@ const mongoose = require("mongoose");
 
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profileImageUrl: { type: String, default: null },
-}, {
-    timestamps: true
-}
-)
-
+const userSchema = new mongoose.Schema(
+    {
+        fullName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        profileImageUrl: { type: String, default: null },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 // Hash password before saving
 userSchema.pre("save", async function (next) {
@@ -20,13 +21,11 @@ userSchema.pre("save", async function (next) {
     }
     this.password = await bcrypt.hash(this.password, 10);
     next();
-
-    
 });
 
 // compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
-}
+};
 
 module.exports = mongoose.model("User", userSchema);
