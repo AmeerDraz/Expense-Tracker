f24f6a4e5e66969962c458241d8ae5229e48629c
AmeerDraz <ameerabudraz1@gmail.com>
2025-09-03 16:19:33 +0300

    feat: TransactionInfoCard

--- frontend/src/components/Cards/TransactionInfoCard.jsx
+++ frontend/src/components/Cards/TransactionInfoCard.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+    LuUtensils,
+    LuTrendingUp,
+    LuTrendingDown,
+    LuTrash2
+} from 'react-icons/lu'
 
 const TransactionInfoCard = ({
     key,
@@ -9,7 +15,13 @@ const TransactionInfoCard = ({
     type,
     hideDeleteBtn,
 }) => {
-    return <div>TransactionInfoCard</div>;
+    return <div className="group relative flex items-center gap-4 mt-2 p-3 rounded-lg">
+        <div className="w-12 h-12 flex items-center justify-center text-xl text-gray-800 bg-gray-100 rounded-full">
+            {icon ? (
+            <img src={icon} alt={title} className="w-6 h-6" />
+            ):(<LuUtensils />)}
+        </div>
+    </div>;
 };
 
 export default TransactionInfoCard;
