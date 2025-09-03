c7a0c058cccd5727cdfec0ba03aba10378fca9b9
AmeerDraz <ameerabudraz1@gmail.com>
2025-09-03 16:07:19 +0300

    feat: RecentTransactions

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = ({ activeMenu }) => {
     return (
         <div className="flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
             <button
-                className="block lg:hidden text-black" onClick={() => setOpenSideMenu(!openSideMenu)}>
+                className="block min-[1080px]:hidden text-black"
+                onClick={() => setOpenSideMenu(!openSideMenu)}
+            >
                 {openSideMenu ? (
                     <HiOutlineX className="text-2xl" />
                 ) : (
@@ -22,7 +24,6 @@ const Navbar = ({ activeMenu }) => {
                     <SideMenu activeMenu={activeMenu} />
                 </div>
             )}
-
         </div>
     );
 };
