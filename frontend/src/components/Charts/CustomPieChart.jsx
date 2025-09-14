9b162c28be0b0e66941182c5d60afc794eaf3c37
AmeerDraz <ameerabudraz1@gmail.com>
2025-09-14 13:27:39 +0300

    fix CustomTooltip test3

--- frontend/src/components/Charts/CustomPieChart.jsx
+++ frontend/src/components/Charts/CustomPieChart.jsx
@@ -6,8 +6,8 @@ import {
     ResponsiveContainer,
     Legend,
 } from "recharts";
-import CustomLegend from "./CustomLegend.jsx";
-import CustomTooltip from "./CustomTooltip.jsx";
+import CustomLegend from "./CustomLegend";
+import CustomTooltip from './CustomTooltip';
 
 const CustomPieChart = ({
     data,
